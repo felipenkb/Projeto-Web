523810bf905462d18320ff252aad936ad9212c70
Fabio Fellipe <fabio.fellipe99@gmail.com>
2021-12-04 15:13:44 -0300

    Incluindo CRUD Scaffolding

--- ProjetoWeb/Startup.cs
+++ ProjetoWeb/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWeb.Data;
 
 namespace ProjetoWeb
 {
@@ -33,6 +35,9 @@ namespace ProjetoWeb
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
